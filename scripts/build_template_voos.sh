ec011dac69dcbee08999dbae40e9e9f543a47e1c
marcus-moura <marcuspaulo.moura@hotmail.com>
2024-02-16 18:26:47 -0300

    ci: create logic for trigger ci with change file

--- scripts/build_template_voos.sh
+++ scripts/build_template_voos.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Variáveis de ambiente
-TEMPLATE_MODULE="../classic_templates/template_voos/pipe_voos_to_bigquery.py"
-TEMPLATE_NAME="batch_job_df_gcs_big_query_template"
-TABLE_NAME="new_table"
-DATASET_NAME="raw_curso_dataflow_voos"
-DATA_SOURCE="voos_sample.csv"
-
 # Preparação do template
 python $TEMPLATE_MODULE \
     --project_id $PROJECT_ID \
